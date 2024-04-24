0fb07f480652d66190f0079726e989480a091fcb
Michael Hellon <kansasboy308@gmail.com>
2024-04-24 13:12:48 -0500

    added light dark mode button updated script links Changes to be committed: modified:   assets/css/blog.css modified:   assets/css/form.css modified:   assets/css/style.css modified:   assets/js/logic.js modified:   blog.html modified:   index.html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,3 +7,8 @@ light and dark mode
 common.js / function element for both pages // qs for both
 
 */
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
